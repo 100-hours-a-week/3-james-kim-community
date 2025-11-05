165989e07794a67ed5e75825456c36978db62fe4
Jongmin Kim <jmkim990226@gmail.com>
2025-11-06 08:52:40 +0900

    fix: 배포 url 변경

--- frontend/public/js/config/api.js
+++ frontend/public/js/config/api.js
@@ -2,10 +2,10 @@
 // 백엔드 API 엔드포인트 URL 관리
 
 // API 기본 URL (스프링부트 서버)
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://ec2-43-200-174-164.ap-northeast-2.compute.amazonaws.com:8080/api';
 
 // 이미지 파일 접근용 (로컬 파일 시스템이라서 임시 - s3 전환 후 수정 예정)
-const BACKEND_BASE_URL = 'http://localhost:8080';
+const BACKEND_BASE_URL = 'http://ec2-43-200-174-164.ap-northeast-2.compute.amazonaws.com:8080:8080';
 
 // 로그인 API 엔드포인트
 const API_ENDPOINTS = {
